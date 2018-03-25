7b28687858835918cf940039068e792ccf0fcc59
Theophilus Omorebee <theo4u@ymail.com>
2018-03-25 15:18:41 +0100

    added my dotfiles

---
+++ zsh.sh
@@ -0,0 +1,4 @@
+##################
+# Following  installation from the repo's Readme
+#################
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
