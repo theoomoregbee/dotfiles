a4b26654acb29d4b37f6757c8f0ee7dd19ad4561
Theophilus Omorebee <theo4u@ymail.com>
2018-03-26 03:42:25 +0100

    fix sym link

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -9,7 +9,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bash_profile vimrc vim zshrc"    # list of files/folders to symlink in homedir
+files=".bash_profile .vimrc .vim .zshrc"    # list of files/folders to symlink in homedir
 
 ##########
 
@@ -26,7 +26,7 @@ echo "done"
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
 for file in $files; do
     echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file ~/dotfiles_old/
+    mv ~/$file ~/dotfiles_old/
     echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/.$file
+    ln -s $dir/.$file ~/$file
 done
